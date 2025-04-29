2e052e5f89b0e61fa7a624ec4b03015ba24430da
KaitlynMorriss <kam4t9@umsystem.edu>
2025-04-29 01:03:59 -0500

    update

---
+++ js/gallery.js
@@ -0,0 +1,44 @@
+
+"use strict";
+
+var photoDivs = document.getElementsByClassName("gallery-image");
+
+var nextButton = document.getElementById("button-next");
+
+var previousButton = document.getElementById("button-previous");
+
+var photoNumber = document.getElementById("stepper");
+
+var currentPhotoNumber = 0;
+
+photoDivs[currentPhotoNumber].classList.remove('hideThis');
+
+photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
+
+nextButton.addEventListener('click', function() {
+  photoDivs[currentPhotoNumber].classList.add("hideThis");
+  currentPhotoNumber = currentPhotoNumber + 1;
+
+  if (currentPhotoNumber === photoDivs.length) {
+    currentPhotoNumber = 0;
+  }
+
+  photoDivs[currentPhotoNumber].classList.remove('hideThis');
+
+  photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
+
+});
+
+previousButton.addEventListener('click', function() {
+  photoDivs[currentPhotoNumber].classList.add("hideThis");
+  currentPhotoNumber = currentPhotoNumber - 1;
+
+  if (currentPhotoNumber < 0) {
+    currentPhotoNumber = photoDivs.length - 1;
+  }
+
+  photoDivs[currentPhotoNumber].classList.remove('hideThis');
+
+  photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
+
+});
